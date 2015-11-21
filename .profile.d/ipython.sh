80491e6cd27463e83d8d5fd19c17102e55d7c154
Troy Shu <tmshu1@gmail.com>
2015-11-21 15:11:23 -0500

    configure ipython

--- .profile.d/ipython.sh
+++ .profile.d/ipython.sh
@@ -3,3 +3,7 @@ source '/app/.profile.d/python.sh'
 if [[ -n "${IPYTHON_PASSWORD:+_}" ]]; then
 	export IPYTHON_PASSWORD_HASH=$( python -c "from IPython.lib import passwd; print passwd('${IPYTHON_PASSWORD}')" )
 fi
+
+# move ipython_notebook_config.py to correct directory
+mkdir .ipython/profile_default
+mv ipython_notebook_config.py .ipython/profile_default/ipython_notebook_config.py
